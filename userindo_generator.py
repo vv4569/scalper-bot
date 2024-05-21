faf662716dfc524f4dbcb53196237f07ba0c0f4e
KelvinWongML <87269110+KelvinWongML@users.noreply.github.com>
2024-05-21 08:17:36 +0800

    Update userindo_generator.py
    -Cooperate with email_registration.py

--- userindo_generator.py
+++ userindo_generator.py
@@ -1,4 +1,5 @@
 from random import choice 
+from email_registration import email_registration
 
 alphabetUP = 'QWERTYUIOPASDFGHJKLZXCVBNM'
 alphabetLOW = 'qwertyuiopasdfghjklzxcvbnm'
@@ -8,7 +9,7 @@ symbol = '!#$%_.+~`<>?'
 def random_name_pw(username_digits: str = 12, password_digits: str = 8) -> list[str, str, str, str]:
     """
     Usage:
-    Generate randomized username and password
+    Generate randomized username and password.
 
     Arguments:
     username_digits: str = digits of randomized username. Defaults to 12.
@@ -18,27 +19,33 @@ def random_name_pw(username_digits: str = 12, password_digits: str = 8) -> list[
     List[str, str, str, str] = [username, gmail, password, pin8]
     """
     if not isinstance(username_digits, int) or not isinstance(password_digits, int): raise ValueError('username_digits and password_digits have to be integers.')
-    if username_digits < 4: raise ValueError('Value of username_digits cannot be less than 4.')
+    if username_digits < 8: raise ValueError('Value of username_digits cannot be less than 8.')
     if password_digits < 8: raise ValueError('Value of password_digits cannot be less than 8.')
 
     result: list[str, str, str, str] = ['', '', '', '']
 
-    result[0] = ''.join([choice(alphabetUP+alphabetLOW+number) for _ in range(username_digits)])
-    if username_digits >= 8:
-        result[1] = result[0]+'@gmail.com'
-    else:
-        result[1] = result[0]+''.join([choice(alphabetUP+alphabetLOW+number) for _ in range(8-username_digits)])+'@gmail.com'
-    result[2] = choice(alphabetUP)+choice(symbol)+choice(number)+''.join([choice(alphabetLOW) for _ in range(password_digits-3)])
+    result[0] = choice(alphabetUP + alphabetLOW) + ''.join([choice(alphabetUP + alphabetLOW + number) for _ in range(username_digits-1)])
+    result[2] = choice(alphabetUP) + choice(symbol) + choice(number) + ''.join([choice(alphabetLOW) for _ in range(password_digits-3)])
     result[3] = ''.join([choice(number) for _ in range(8)])
+    
+    if email_registration(name = result[0], password = result[1]) == True:
+        result[1] = result[0] + '@outlook.com'
 
     return result
 
 
 if __name__ == "__main__":
-    test_case = [[12, 12], [8, 8], [4, 8], [1, 2], [4, 1], ['p', 9], [9, 'p'], ['p', 'p']]
-    for num, [ud, pd] in enumerate(test_case):
+    test_case = [[12, 12], 
+                 [8, 8], 
+                 [4, 8], 
+                 [1, 2], 
+                 [4, 1], 
+                 ['p', 9], 
+                 [9, 'p'], 
+                 ['p', 'p']]
+    for num, [ud, pd, pn] in enumerate(test_case):
         print(f'TEST CASE {num}:  ', end='')
         try:
-            print(random_name_pw(ud, pd))
+            print(random_name_pw(ud, pd, pn))
         except Exception as e:
             print(e)
