44f353584d2a7b57b65032bcbdbf53f98208d511
KelvinWongML <87269110+KelvinWongML@users.noreply.github.com>
2024-05-21 08:19:14 +0800

    Update db.py
    -Cooperate with userindo_generator,py

--- db.py
+++ db.py
@@ -18,10 +18,15 @@ engine = create_engine("sqlite:///database/main.db", echo=False)
 
 # initializes the database
 
+#Basic setup
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
+failed: int = 0
+test_mode: bool = bool(int(input('Test mode (0/1): ')))
+phone_no = str(input('Phone number: '))
 
+#Table format
 class User(Base):                       
     __tablename__ = 'users'             
     id = Column(Integer, primary_key=True)
@@ -31,8 +36,11 @@ class User(Base):
     pin8 = Column(String)
 
 Base.metadata.create_all(bind=engine)
-session.query(User).delete()            #Emptying table. For testing purpose. Need to be removed
 
+#Emptying table. For testing purpose. Need to be removed.
+session.query(User).delete()            
+
+#Generate users.
 num_users: int = 0
 while True:
     try:
@@ -40,13 +48,22 @@ while True:
         break  
     except ValueError:
         print("Invalid input. Please enter an integer.")
+
 for _ in tqdm(range(num_users)): 
-    result: list[str, str, str, str] = random_name_pw(username_digits=12, password_digits=12)
+    result: list[str, str, str, str] = random_name_pw(username_digits=12, password_digits=12, phone_no=phone_no)
+    if result[1] == '':
+        failed += 1
+        print(f'Name: {result[0]}, email registration failed.')
+        continue
     session.add(User(username=result[0], gmail=result[1], password=result[2], pin8=result[3]))
+
+print(f'Successfully create {num_users-failed} yahoo mail account(s)....({num_users-failed}/{num_users})')
 session.commit()
 
-users = session.query(User).all()
-for user in users:
-    print(user.id, user.username, user.gmail, user.password, user.pin8)
+#Show output.
+if test_mode:           
+    users = session.query(User).all()
+    for user in users:
+        print(user.id, user.username, user.gmail, user.password, user.pin8)
 
 session.close()
